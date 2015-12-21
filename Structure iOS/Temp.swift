7343ec377e485840c68d586b58112815c8789409
Stephen H. Gerstacker <stephen@gerstacker.us>
2015-12-20 22:36:48 -0500

    Initial build for iOS

---
+++ Structure iOS/Temp.swift
@@ -0,0 +1,10 @@
+//
+//  Temp.swift
+//  Structure
+//
+//  Created by Stephen Gerstacker on 12/20/15.
+//  Copyright © 2015 Stephen H. Gerstacker. All rights reserved.
+//
+
+import Foundation
+import sqlite3
--- Structure/Structure.h
+++ Structure/Structure.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double StructureVersionNumber;
 FOUNDATION_EXPORT const unsigned char StructureVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Structure/PublicHeader.h>
-
-
