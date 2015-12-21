4b269dcf976f9f6a4c7a8b735bfd4b3efd53a670
Stephen H. Gerstacker <stephen@gerstacker.us>
2015-12-20 22:09:21 -0500

    Initial Commit

---
+++ Structure/Structure.h
@@ -0,0 +1,19 @@
+//
+//  Structure.h
+//  Structure
+//
+//  Created by Stephen Gerstacker on 12/20/15.
+//  Copyright © 2015 Stephen H. Gerstacker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Structure.
+FOUNDATION_EXPORT double StructureVersionNumber;
+
+//! Project version string for Structure.
+FOUNDATION_EXPORT const unsigned char StructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Structure/PublicHeader.h>
+
+
