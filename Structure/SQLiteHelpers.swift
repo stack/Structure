e78fc1f02fa74bdd6148f5b6f484dddc7eaad68c
Stephen H. Gerstacker <stephen@gerstacker.us>
2016-01-01 20:25:51 -0500

    Updated copyrights to 2016

--- Structure/SQLiteHelpers.swift
+++ Structure/SQLiteHelpers.swift
@@ -3,7 +3,7 @@
 //  Structure
 //
 //  Created by Stephen Gerstacker on 12/20/15.
-//  Copyright © 2015 Stephen H. Gerstacker. All rights reserved.
+//  Copyright © 2016 Stephen H. Gerstacker. All rights reserved.
 //
 
 import sqlite3
