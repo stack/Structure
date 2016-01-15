c7da5f2de78a9d03ce8ba460501cadde3c0befce
Stephen H. Gerstacker <stephen@gerstacker.us>
2016-01-14 22:36:45 -0500

    Mac framework target

---
+++ Structure Mac/Structure.h
@@ -0,0 +1,19 @@
+//
+//  Structure.h
+//  Structure Mac
+//
+//  Created by Stephen Gerstacker on 1/14/16.
+//  Copyright © 2016 Stephen H. Gerstacker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Structure Mac.
+FOUNDATION_EXPORT double Structure_MacVersionNumber;
+
+//! Project version string for Structure Mac.
+FOUNDATION_EXPORT const unsigned char Structure_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Structure_Mac/PublicHeader.h>
+
+
