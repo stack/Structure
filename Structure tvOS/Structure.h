52a83ffda32271dd528e42d34f133c85849c9363
Stephen H. Gerstacker <stephen@gerstacker.us>
2016-01-14 22:46:16 -0500

    Added the tvOS target

---
+++ Structure tvOS/Structure.h
@@ -0,0 +1,19 @@
+//
+//  Structure tvOS.h
+//  Structure tvOS
+//
+//  Created by Stephen Gerstacker on 1/14/16.
+//  Copyright © 2016 Stephen H. Gerstacker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Structure tvOS.
+FOUNDATION_EXPORT double Structure_tvOSVersionNumber;
+
+//! Project version string for Structure tvOS.
+FOUNDATION_EXPORT const unsigned char Structure_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Structure_tvOS/PublicHeader.h>
+
+
